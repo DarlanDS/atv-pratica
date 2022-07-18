9f2e280794722387add02eae0bb0fecc2a235d25
darlans152@gmail.com <darlans152@gmail.com>
2022-07-18 07:15:50 -0300

    sessao sobre e formacao

--- static/script.js
+++ static/script.js
@@ -5,7 +5,20 @@ const body = document.querySelector('body');
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-card-list")
-        ? menuMobile.classList.replace("bi-card-list","bi-x")
+        ? menuMobile.classList.replace("bi-card-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-card-list");
     body.classList.toggle("menu-nav-active");
 });
+
+/* Fechar o menu ao clicar o icone lista*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-nav-active')) {
+            body.classList.remove('menu-nav-active')
+            menuMobile.classList.replace("bi-x", "bi-card-list")
+        }
+    })
+})
